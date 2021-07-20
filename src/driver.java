849e13f55f1f26636695dd44e0359f37dfbc5724
PJWrobel <wrobel.paulj@gmail.com>
2021-07-20 17:13:17 -0600

    java GUI

---
+++ src/driver.java
@@ -0,0 +1,11 @@
+import system.in
+import somethingelse
+
+class driver{
+    public static void main(String[] args){
+        Panel jpane = new Pane();
+        jpane.start();
+        System.out.println("closing");
+        return 0;
+    }
+}
